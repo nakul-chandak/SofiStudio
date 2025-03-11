448c91f3d657d44e734eafa66a71719bf096101d
nakul-chandak <chandak.nakul21@gmail.com>
2025-03-11 17:50:57 +0530

    Added User Route
    Added User Route

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -6,7 +6,7 @@ export const defaultNavigation: FuseNavigationItem[] = [
         id   : 'example',
         title: 'Home',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:home',
         link : '/example'
     }
 ];
@@ -15,7 +15,7 @@ export const compactNavigation: FuseNavigationItem[] = [
         id   : 'example',
         title: 'Home',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:home',
         link : '/example'
     }
 ];
@@ -24,7 +24,7 @@ export const futuristicNavigation: FuseNavigationItem[] = [
         id   : 'example',
         title: 'Home',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:home',
         link : '/example'
     }
 ];
@@ -33,7 +33,14 @@ export const horizontalNavigation: FuseNavigationItem[] = [
         id   : 'example',
         title: 'Home',
         type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
+        icon : 'heroicons_outline:home',
         link : '/example'
+    },
+    {
+        id   : 'user',
+        title: 'Users',
+        type : 'basic',
+        icon : 'heroicons_outline:users',
+        link : '/apps/contacts'
     }
 ];
